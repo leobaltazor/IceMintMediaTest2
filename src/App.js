15dd435a071d1788cdfc81015b6d4e48935af1ff
Leo <leo.baltazor@gmail.com>
2018-08-28 01:18:31 +0300

    start get img

--- src/App.js
+++ src/App.js
@@ -3,9 +3,32 @@ import "./App.css";
 
 import getMovie from "./actions/movie";
 
-getMovie()
 class App extends Component {
+    state = {
+        movie: [],
+        shows: null,
+        isLoading: false
+    };
+    async componentDidMount() {
+        this.setState({ isLoading: !this.state.isLoading });
+        let movie = await getMovie();
+        this.setState({
+            ...this.state,
+            movie: movie.results
+        });
+        for (let i = 1; i < movie.total_pages; ++i) {
+            let element = await getMovie(i + 1);
+            element = element.results;
+            element = [...element.map(e => e), ...this.state.movie];
+            this.setState({
+                ...this.state,
+                movie: element
+            });
+        }
+        this.setState({ isLoading: !this.state.isLoading });
+    }
     render() {
+        console.log(this.state.movie.map(e=> e.backdrop_path));
         return (
             <div className="App">
                 <header className="App-header">
